2399c967a4d4222bbd1ace11c608efa36f3adeff
Vinncent Miller <vinncentmiller@gmail.com>
2024-10-05 18:53:09 -0400

    Serverless for Vercel

--- api/jobs.js
+++ api/jobs.js
@@ -1,5 +1,6 @@
 export default async function handler(req, res) {
     const apiKey = import.meta.env.VITE_API_KEY;
+  
     try {
       const response = await fetch('https://findwork.dev/api/jobs/', {
         method: 'GET',
@@ -10,6 +11,7 @@ export default async function handler(req, res) {
       });
   
       if (!response.ok) {
+        console.error(`Error fetching data: ${response.status} ${response.statusText}`);
         res.status(response.status).json({ error: 'Failed to fetch data' });
         return;
       }
@@ -18,6 +20,7 @@ export default async function handler(req, res) {
       res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins (adjust if needed)
       res.status(200).json(data);
     } catch (error) {
+      console.error('Server error:', error);
       res.status(500).json({ error: 'Server error' });
     }
   }
